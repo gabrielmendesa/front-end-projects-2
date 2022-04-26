3bbf8b93d5e336c52f72ea495cf0a0de6a6c8af4
Vinicius Gomes <viniplay0599@gmail.com>
2022-04-26 16:23:53 -0300

    Configuração inicial

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import React from 'react';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import GlobalProvider from './context/globalProvider';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <GlobalProvider>
+      <div className="meals">
+        <span className="logo">TRYBE</span>
+        <object
+          className="rocksGlass"
+          type="image/svg+xml"
+          data={ rockGlass }
+        >
+          Glass
+        </object>
+      </div>
+    </GlobalProvider>
   );
 }
 
 export default App;
+
+// npm run lint -- --fix
