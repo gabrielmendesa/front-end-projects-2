d6ae1774b6d5cf0a86cf10d60100681195fc0a2c
Vinicius Gomes <viniplay0599@gmail.com>
2022-04-27 14:37:51 -0300

    Login CSS

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faKey } from '@fortawesome/free-solid-svg-icons';
+import burger from '../img/burger.svg';
+import '../styles/Login.css';
 
 export default function Login({ history }) {
   const [stateEmail, setStateEmail] = useState('');
@@ -31,32 +36,52 @@ export default function Login({ history }) {
 
   return (
     <div className="login-container">
-      <input
-        placeholder="Email"
-        className="email-input"
-        data-testid="email-input"
-        type="email"
-        onChange={ handleChange }
-        value={ stateEmail }
-        name="email"
-      />
-      <input
-        placeholder="Senha"
-        className="password-input"
-        data-testid="password-input"
-        type="password"
-        onChange={ handleChange }
-        value={ statePassword }
-        name="password"
-      />
-      <button
-        type="submit"
-        data-testid="login-submit-btn"
-        disabled={ isDisable }
-        onClick={ handleCLick }
-      >
-        Enter
-      </button>
+      <img src={ burger } alt="Logo" className="login-image" />
+      <p className="login-text">
+        <span>SING IN</span>
+        <span>TO CONTINUE</span>
+      </p>
+      <div className="login-form">
+        <div className="textfield">
+          <label htmlFor="email-input">
+            <FontAwesomeIcon icon={ faUser } className="login-icon" />
+            Email
+            <input
+              placeholder="Digite seu email"
+              className="email-input"
+              data-testid="email-input"
+              type="email"
+              onChange={ handleChange }
+              value={ stateEmail }
+              name="email"
+            />
+          </label>
+        </div>
+        <div className="textfield">
+          <label htmlFor="password-input">
+            <FontAwesomeIcon icon={ faKey } className="login-icon" />
+            Senha
+            <input
+              placeholder="Digite sua senha"
+              className="password-input"
+              data-testid="password-input"
+              type="password"
+              onChange={ handleChange }
+              value={ statePassword }
+              name="password"
+            />
+          </label>
+        </div>
+        <button
+          className="login-btn"
+          type="submit"
+          data-testid="login-submit-btn"
+          disabled={ isDisable }
+          onClick={ handleCLick }
+        >
+          Enter
+        </button>
+      </div>
     </div>
   );
 }
