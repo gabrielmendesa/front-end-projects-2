f588c65c43dd5246865e4a8f83befa6e1d3982ac
Thiago William <thiagokepler1@gmail.com>
2022-04-28 20:50:09 -0300

    requisitos 17

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/Header';
+import globalContext from '../context/globalContext';
 
 export default function Drinks() {
+  function renderRecipes(recipe, i) {
+    const num = 12;
+    if (i >= num) return '';
+    return (
+      <div data-testid={ `${i}-recipe-card` } key={ i }>
+        <img
+          src={ recipe.strDrinkThumb }
+          alt={ recipe.strDrink }
+          data-testid={ `${i}-card-img` }
+        />
+        <p data-testid={ `${i}-card-name` }>{recipe.strDrink}</p>
+      </div>
+    );
+  }
+
+  const { recipes } = useContext(globalContext);
   return (
     <div>
       <Header titleName="Drinks" />
-      <h1>Tela bebidas</h1>
+      { recipes.drinks && recipes.drinks.map((recipe, i) => renderRecipes(recipe, i)) }
     </div>
   );
 }
--- src/pages/Food.js
+++ src/pages/Food.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/Header';
+import globalContext from '../context/globalContext';
 
 export default function Food() {
+  const { recipes } = useContext(globalContext);
+
   return (
     <div>
       <Header titleName="Foods" />
+      { recipes.meals && recipes.meals.map((recipe, i) => (
+        <div data-testid={ `${i}-recipe-card` } key={ i }>
+          <img
+            src={ recipe.strMealThumb }
+            alt={ recipe.strMeal }
+            data-testid={ `${i}-card-img` }
+          />
+          <p data-testid={ `${i}-card-name` }>{recipe.strMeal}</p>
+        </div>
+      ))}
     </div>
   );
 }
