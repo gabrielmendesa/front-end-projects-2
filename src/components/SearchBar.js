305efd1dc4ca713a66fa1959e735eb25aeb8ede6
Thiago William <thiagokepler1@gmail.com>
2022-04-28 17:58:22 -0300

    requisitos 14 e 15

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,57 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import globalContext from '../context/globalContext';
+import fetchAPI from '../services/fetchAPI';
 
 export default function SearchBar() {
+  const { setRecipes } = useContext(globalContext);
+  const history = useHistory();
+  const [filter, setFilter] = useState('');
+  const [valueInput, setValueInput] = useState('');
+
+  function checkURL() {
+    const { pathname } = history.location;
+    const urlParam = pathname === '/foods' ? 'themealdb' : 'thecocktaildb';
+    const ingredientUrl = `https://www.${urlParam}.com/api/json/v1/1/filter.php?i=${valueInput}`;
+    const nameUrl = `https://www.${urlParam}.com/api/json/v1/1/search.php?s=${valueInput}`;
+    const firstLetter = `https://www.${urlParam}.com/api/json/v1/1/search.php?f=${valueInput}`;
+
+    switch (filter) {
+    case 'ingredient':
+      return ingredientUrl;
+    case 'name':
+      return nameUrl;
+    case 'firstLetter':
+      return valueInput.length > 1
+        ? global.alert('Your search must have only 1 (one) character')
+        : firstLetter;
+    default:
+      return valueInput;
+    }
+  }
+
+  async function handleClick() {
+    const URL = checkURL();
+    const recipes = await fetchAPI(URL);
+    setRecipes(recipes);
+  }
+
   return (
     <div>
-      <input type="text" data-testid="search-input" className="search-input" />
+      <input
+        type="text"
+        data-testid="search-input"
+        className="search-input"
+        onChange={ ({ target }) => setValueInput(target.value) }
+      />
       <div>
         <label htmlFor="radio-button1">
           <input
             name="filter-radio"
             type="radio"
             id="radio-button1"
+            value="ingredient"
+            onChange={ ({ target }) => setFilter(target.value) }
             data-testid="ingredient-search-radio"
           />
           Ingredient
@@ -19,6 +61,8 @@ export default function SearchBar() {
           <input
             name="filter-radio"
             type="radio"
+            value="name"
+            onChange={ ({ target }) => setFilter(target.value) }
             id="radio-button2"
             data-testid="name-search-radio"
           />
@@ -28,6 +72,8 @@ export default function SearchBar() {
           <input
             name="filter-radio"
             type="radio"
+            value="firstLetter"
+            onChange={ ({ target }) => setFilter(target.value) }
             id="radio-button3"
             data-testid="first-letter-search-radio"
           />
@@ -37,6 +83,7 @@ export default function SearchBar() {
       <button
         type="button"
         data-testid="exec-search-btn"
+        onClick={ handleClick }
       >
         Search
       </button>
