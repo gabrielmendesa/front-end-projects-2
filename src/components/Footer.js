ff608946318c342120a8806ab5e6efca4dd1b898
Thiago William <thiagokepler1@gmail.com>
2022-04-29 13:38:23 -0300

    requisito 22

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,36 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 
 function Footer() {
-  const { push } = useHistory();
-  console.log(push);
   return (
     <div data-testid="footer" className="footer">
-      <img
-        className="drinks-bottom-btn"
-        data-testid="drinks-bottom-btn"
-        src={ drinkIcon }
-        alt="drink icon"
-      />
-      <img
-        className="explore-bottom-btn"
-        data-testid="explore-bottom-btn"
-        src={ exploreIcon }
-        alt="explore icon"
-      />
-      <img
-        className="food-bottom-btn"
-        data-testid="food-bottom-btn"
-        src={ mealIcon }
-        alt="meal icon"
-      />
+      <Link to="/drinks">
+        <img
+          className="drinks-bottom-btn"
+          data-testid="drinks-bottom-btn"
+          src={ drinkIcon }
+          alt="drink icon"
+        />
+      </Link>
+      <Link to="/explore">
+        <img
+          className="explore-bottom-btn"
+          data-testid="explore-bottom-btn"
+          src={ exploreIcon }
+          alt="explore icon"
+        />
+      </Link>
+      <Link to="/foods">
+        <img
+          className="food-bottom-btn"
+          data-testid="food-bottom-btn"
+          src={ mealIcon }
+          alt="meal icon"
+        />
+      </Link>
     </div>
   );
 }
